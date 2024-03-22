17facd4d2499ad949bc42c752fcbd4b1535d9723
code1218 <skjil1218@gmail.com>
2024-03-22 17:45:26 +0900

    비밀번호 변경완료

--- src/main/java/com/study/library/controller/AuthController.java
+++ src/main/java/com/study/library/controller/AuthController.java
@@ -8,13 +8,16 @@ import com.study.library.dto.SigninReqDto;
 import com.study.library.dto.SignupReqDto;
 import com.study.library.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +26,8 @@ import java.util.Map;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Value("${spring.security.oauth2.client.registration.kakao.clientId}")
+    private String kakaoClientId;
     @Autowired
     private AuthService authService;
 
@@ -51,6 +56,12 @@ public class AuthController {
     public ResponseEntity<?> oAuth2Merge(@RequestBody OAuth2MergeReqDto oAuth2MergeReqDto) {
         authService.oAuth2Merge(oAuth2MergeReqDto);
         return ResponseEntity.ok(true);
+    }
+
+    @GetMapping("/oauth2/logout")
+    public ResponseEntity<?> oAuth2Logout(HttpServletResponse response) throws IOException {
+        response.sendRedirect("https://kauth.kakao.com/oauth/logout?client_id=" + kakaoClientId + "&logout_redirect_uri=http://localhost:3000/auth/logout");
+        return null;
     }
 
 }
